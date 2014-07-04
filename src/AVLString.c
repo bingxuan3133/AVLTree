09be23bb438bc43ffe2071019e8feed5376cb315
bingxuan3133 <bensoncbx@gmail.com>
2014-07-04 23:04:25 +0800

    Clean up and AVLString is pending to be implemented

--- src/AVLString.c
+++ src/AVLString.c
@@ -1,10 +1,5 @@
 #include "AVLString.h"
 
 int compareString(void *nodeInTree, void *nodeToCompare) {
-  Node *node1 = (Node *)nodeInTree;
-  Node *node2 = (Node *)nodeToCompare;
-  
-  if(node1->data == node2->data) return 0;
-  else if(node1->data > node2->data) return 1;
-  else if(node1->data < node2->data) return -1;
+// to be implemented
 }
